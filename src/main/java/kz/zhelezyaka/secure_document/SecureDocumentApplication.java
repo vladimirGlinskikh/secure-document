c90958786fb6103f69d75a789aa7906973d5e1d0
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-06-09 16:21:18 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/secure_document/SecureDocumentApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.secure_document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureDocumentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/zhelezyaka/secure_document/SecureDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.secure_document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
