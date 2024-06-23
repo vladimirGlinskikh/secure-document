6464080a0793b3f40e6243d702b28bc649f8ace3
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-06-23 18:33:50 +0300

    Confirmation Entity

---
+++ src/main/java/kz/zhelezyaka/secure_document/entity/ConfirmationEntity.java
@@ -0,0 +1,50 @@
+package kz.zhelezyaka.secure_document.entity;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.UUID;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_DEFAULT;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "confirmations")
+@JsonInclude(NON_DEFAULT)
+public class ConfirmationEntity extends Auditable {
+    private String key;
+
+    @OneToOne(targetEntity = UserEntity.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonProperty("user_id")
+    private UserEntity userEntity;
+
+    public ConfirmationEntity(UserEntity userEntity) {
+        this.userEntity = userEntity;
+        this.key = UUID.randomUUID().toString();
+    }
+}
