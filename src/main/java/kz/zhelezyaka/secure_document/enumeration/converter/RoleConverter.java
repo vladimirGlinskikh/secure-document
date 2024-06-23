b31669c60909bf4f35c75292e9370993cb34ecf8
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-06-23 18:06:24 +0300

    Role Converter

---
+++ src/main/java/kz/zhelezyaka/secure_document/enumeration/converter/RoleConverter.java
@@ -0,0 +1,29 @@
+package kz.zhelezyaka.secure_document.enumeration.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import kz.zhelezyaka.secure_document.enumeration.Authority;
+
+import java.util.stream.Stream;
+
+@Converter(autoApply = true)
+public class RoleConverter implements AttributeConverter<Authority, String> {
+    @Override
+    public String convertToDatabaseColumn(Authority authority) {
+        if (authority == null) {
+            return null;
+        }
+        return authority.getValue();
+    }
+
+    @Override
+    public Authority convertToEntityAttribute(String code) {
+        if (code == null) {
+            return null;
+        }
+        return Stream.of(Authority.values())
+                .filter(authority -> authority.getValue().equals(code))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
